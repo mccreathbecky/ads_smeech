b98095a450056b6a355b57b99c0019ba297643b7
mccreathbecky <mccreathbecky@gmail.com>
2016-09-15 12:42:31 +1000

    added input representing consumption for house

--- ControlUnit.vhd
+++ ControlUnit.vhd
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: UTS
+-- Engineer: Becky + Will
 -- 
 -- Create Date:    12:24:43 09/15/2016 
 -- Design Name: 
@@ -19,27 +19,42 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity ControlUnit is
-    Port ( ManualControl : in  STD_LOGIC;
+    Port ( 
+           -- used by the user to switch on manual control
+           ManualControl : in  STD_LOGIC;
+           
+           -- a switch that is only active once manualcontrol is on 
+           -- '1' is solar, '0' is grid
            SourceSelect : in  STD_LOGIC;
-           SolarIn : in  STD_LOGIC_VECTOR (13 downto 0);
-           Mains : in  STD_LOGIC_VECTOR (13 downto 0);
+           
+           -- up to 5000Wh - 13 bit
+           SolarIn : in  STD_LOGIC_VECTOR (12 downto 0);
+           
+           -- assume 5000Wh - 13 bit
+           Mains : in  STD_LOGIC_VECTOR (12 downto 0);
+           
+           -- power consumption for house - assume no greater than 3kWh  = 12 bit
+           Consumption : in STD_LOGIC_VECTOR (11 downto 0);
+           
+           -- '1'/on when solar is chosen input
            SolarLED : out  STD_LOGIC;
+           
+           -- '1'/on when grid is chosen input
            GridLED : out  STD_LOGIC;
+           
+           -- used to display battery level as a %age
+           -- total power generated for day, solar level as a %age
            SSD_Display : out  STD_LOGIC_VECTOR (6 downto 0));
 end ControlUnit;
 
 architecture Behavioral of ControlUnit is
+
+   -- 
 
 begin
 
