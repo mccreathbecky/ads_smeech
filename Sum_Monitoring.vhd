e4b2148662581d410909f819e0888650299f05f1
mccreathbecky <mccreathbecky@gmail.com>
2016-10-07 19:40:52 +1100

    trying to simulate inout - changed sum_flag to be IN only - not working

--- Sum_Monitoring.vhd
+++ Sum_Monitoring.vhd
@@ -16,6 +16,8 @@
 -- Revision 0.01 - File Created
 -- Additional Comments: 
 --https://www.altera.com/support/support-resources/design-examples/design-software/vhdl/v_bidir.tablet.highResolutionDisplay.html
+--http://www.bitweenie.com/listings/vhdl-type-conversion/
+--http://www.thecodingforums.com/threads/bidirectional-port-usage-in-vhdl.22628/
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -24,11 +26,12 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity Sum_Monitoring is
     Port ( 
-           sum_flag : INOUT  STD_LOGIC;
            battery_sum : INOUT  STD_LOGIC_VECTOR (10 DOWNTO 0);         -- battery sum less than 2047 [max 1500Wh]
            consumption_sum : INOUT  STD_LOGIC_VECTOR (12 DOWNTO 0);     -- consumption_sum assumed less than 8191Wh
            solar_sum : INOUT  STD_LOGIC_VECTOR (11 DOWNTO 0);           -- max solar sum for the day assumed less than 4095Wh
            
+           sum_flag : IN  STD_LOGIC;
+           CLK : IN STD_LOGIC;
            current_source : IN  STD_LOGIC_VECTOR (1 DOWNTO 0);       
            consumption : IN  STD_LOGIC_VECTOR (10 DOWNTO 0);            -- consumption <= battery_sum
            solar_in : IN  STD_LOGIC_VECTOR (9 DOWNTO 0));               -- max solar input is 1000Wh 
@@ -39,16 +42,22 @@ ARCHITECTURE Behavioral OF Sum_Monitoring IS
    CONSTANT sample_rate : UNSIGNED(1 DOWNTO 0) := "10";        -- 2 minute interval
    CONSTANT mains : UNSIGNED(9 DOWNTO 0) := "1111101000";      -- 1000Wh constant
    
-   SIGNAL new_batterysum : UNSIGNED(10 DOWNTO 0);
-   SIGNAL new_consumptionsum : UNSIGNED (12 DOWNTO 0);
-   SIGNAL new_solarsum : UNSIGNED(11 DOWNTO 0);
+   SIGNAL new_batterysum : UNSIGNED(10 DOWNTO 0) := UNSIGNED(battery_sum);
+   SIGNAL new_consumptionsum : UNSIGNED (12 DOWNTO 0) := UNSIGNED(consumption_sum);
+   SIGNAL new_solarsum : UNSIGNED(11 DOWNTO 0) := UNSIGNED(solar_sum);
    
-
+   SIGNAL update_sum : STD_LOGIC := '0';
 
 BEGIN
+   -- TRYING TO FOLLOW THE FORM
+   --  Z: inout
+   -- signal A, OE
+   --Z <= A when OE = '1' else (others => 'Z');
+
+
    -- update solar, battery, consumption sums after monitorINg [for the 2 mIN INterval]
    -- assumes all INputs are IN Wh [not kWh] and divides by 60 to fINd 1 mIN INterval, then * sample_rate
-   sum_monitoring : PROCESS (sum_flag)
+   sum_monitoring : PROCESS (CLK, sum_flag)
    BEGIN
       -- wait for sum_flag to be set
       IF sum_flag ' EVENT AND sum_flag = '1' THEN
@@ -71,11 +80,28 @@ BEGIN
                new_batterysum <= UNSIGNED(battery_sum) - UNSIGNED(consumption)*sample_rate/60 + UNSIGNED(solar_in)*sample_rate/60;
                new_solarsum <= UNSIGNED(solar_sum) + UNSIGNED(solar_in)*sample_rate/60;
          END CASE;
+         
+         update_sum <= '1';
+         
+      ELSE     
+         battery_sum <= (others => 'Z');          
+         consumption_sum <= (others => 'Z');
+         solar_sum <= (others => 'Z');
       END IF;
-      
-      -- reset sum_flag to 0
-      sum_flag <= '0';
    END PROCESS;
+   
+   update_process : PROCESS(update_sum)
+   BEGIN
+      IF update_sum ' EVENT AND update_sum = '1' THEN
+         battery_sum <= STD_LOGIC_VECTOR(new_batterysum);
+         consumption_sum <= STD_LOGIC_VECTOR(new_consumptionsum);
+         solar_sum <= STD_LOGIC_VECTOR(new_solarsum);
+         
+         update_sum <= '0';
+      END IF;
+   END PROCESS;
+   
 
 END Behavioral;
+
 
--- fuseRelaunch.cmd
+++ fuseRelaunch.cmd
@@ -1,1 +1,1 @@
--intstyle "ise" -incremental -o "C:/Users/mccre/OneDrive/Documents/2016/Uni/ADS/Project/SMEECH2/Sum_Monitoring_isim_beh.exe" -prj "C:/Users/mccre/OneDrive/Documents/2016/Uni/ADS/Project/SMEECH2/Sum_Monitoring_beh.prj" "work.Sum_Monitoring" 
+-intstyle "ise" -incremental -o "C:/Users/mccre/OneDrive/Documents/2016/Uni/ADS/Project/SMEECH2/Sum_monitoring_tb_isim_beh.exe" -prj "C:/Users/mccre/OneDrive/Documents/2016/Uni/ADS/Project/SMEECH2/Sum_monitoring_tb_beh.prj" "work.Sum_monitoring_tb" 
